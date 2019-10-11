9f29b6ec0b6894a029b7e3723dc787c500aa7c8c
andrewjknell <andrewjknell@yahoo.com>
2019-10-11 14:38:56 -0700

    end of mongoose

--- Mongoose/TaskAPI/server.js
+++ Mongoose/TaskAPI/server.js
@@ -4,9 +4,6 @@ const express = require('express'),
     path = require('path'),
     app = express();
 
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname + '/client/views'));
 app.use(express.static( __dirname + '/public/dist/public' ));
 
 app.use(express.json());
